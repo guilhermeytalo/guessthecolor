f5ee0038d9a02c46c35e121022599f5fd51643a6
Guilherme Ytalo <gydaytalo@gmail.com>
2023-09-12 17:51:24 -0300

    fix: removed coment and lint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-
   // Tests that the component renders without crashing
   it('should render without crashing', () => {
     render(<App />);
