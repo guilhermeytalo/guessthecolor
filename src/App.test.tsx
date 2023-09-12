16b229812ed8927f50b3375b47bbd7f531d04396
Guilherme Ytalo <gydaytalo@gmail.com>
2023-09-12 17:50:25 -0300

    feat: finished layout and first test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,17 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-    it("Should have hello world", () => {
-        render(<App/>);
-        const message = screen.queryByText(/Hello World/i);
 
-        expect(message).toBeVisible();
-    })
+  // Tests that the component renders without crashing
+  it('should render without crashing', () => {
+    render(<App />);
+  });
+
+  // Tests that the title "Guess the Color!" is displayed
+  it('should display the title "Guess the Color!"', () => {
+    render(<App />);
+    const title = screen.getByText(/Guess the Color!/i);
+    expect(title).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,55 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Guess the Color!</h1>
+
+      <div className="score-board">
+        <div className="remaninin-time-container">
+          <h3>
+            Remaining
+            <br />
+            Time(s)
+          </h3>
+          <p>30</p>
+        </div>
+
+        <div className="restart-container">
+          <button>Restart</button>
+        </div>
+
+        <div className="scores-container">
+          <div className="score">
+            <h3>
+              HIGH
+              <br />
+              SCORE
+            </h3>
+            <p>0</p>
+          </div>
+
+          <div className="high-score">
+            <h3>SCORE</h3>
+            <p>0</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="color-container">
+        <div className="color-name"></div>
+      </div>
+
+      <div className="color-selection-container">
+        <div className="color1">
+          <p>#FF0000</p>
+        </div>
+        <div className="color2">
+          <p>#c98ca7</p>
+        </div>
+
+        <div className="color3">
+          <p>#e76d83</p>
+        </div>
+      </div>
     </div>
   );
 }
